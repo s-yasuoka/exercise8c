7da8058a32a6fd514e7db393a08efb6cac63036d
s-yasuoka <migayasu@gmail.com>
2022-06-03 02:23:45 +0000

    [add]グループ参加

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+  	get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update] do
+    resource :group_users, only: [:create, :destroy]
+    get "join" => "groups#join"
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/search', to: 'searches#search'
+end
